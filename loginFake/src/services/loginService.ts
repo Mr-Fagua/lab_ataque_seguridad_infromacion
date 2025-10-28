9fb9002fc7ab6ba0741091d6d80aedfb71dd3d8b
JVLalejandrofagua <alejandro.fagua@JVLSolutions.onmicrosoft.com>
2025-10-27 22:47:10 -0500

    nuevo endpoint

--- loginFake/src/services/loginService.ts
+++ loginFake/src/services/loginService.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import type { AxiosResponse } from "axios";
 import type { LoginData, LoginResponse } from "../types/AuthTypes";
 
-const API_URL = "http://localhost:3000/api/form";
+const API_URL = "https://lab-ataque-seguridad-infromacion-back.onrender.com/api/form";
 
 export const loginService = async (
   data: LoginData
